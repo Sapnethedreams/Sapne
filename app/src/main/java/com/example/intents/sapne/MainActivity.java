47b08b4bd6614189c2419a7255c4a5f92c6f9876
NamanRawat1 <namanrawat2218@gmail.com>
2017-08-07 12:14:11 +0530

    hamburger icon

--- app/src/main/java/com/example/intents/sapne/MainActivity.java
+++ app/src/main/java/com/example/intents/sapne/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.intents.sapne;
 
-import android.support.design.widget.NavigationView;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Typeface;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static com.example.intents.sapne.R.drawable.lo;
 
 public class MainActivity extends AppCompatActivity  {
 Toolbar toolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +34,67 @@ Toolbar toolbar;
     toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+   getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       getSupportActionBar().setHomeButtonEnabled(true);
 
 
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close ){
+
+
+            @Override
+            public void onDrawerClosed(View view) {
+
+                invalidateOptionsMenu();
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+                invalidateOptionsMenu();
+
+            }
+
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle action buttons
+        return true;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
     }
 
 
 
 
+
+
 }
+
+
+
+
+
