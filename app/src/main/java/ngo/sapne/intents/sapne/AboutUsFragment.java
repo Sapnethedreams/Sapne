1634b1779a552af10ec784abadd6ec1faf561550
rohegde7 <hegde.rohit7@gmail.com>
2017-11-12 01:23:38 +0530

    fragment: OurMission, About us

---
+++ app/src/main/java/ngo/sapne/intents/sapne/AboutUsFragment.java
@@ -0,0 +1,40 @@
+package ngo.sapne.intents.sapne;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rohegde on 11/11/17.
+ */
+
+public class AboutUsFragment extends Fragment{
+
+    /*
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getLayoutInflater().inflate(R.layout.activity_about_us, frameLayout);
+
+
+    }
+    */
+
+    //@Override
+    public View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_about_us, container, false);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        //getLayoutInflater().inflate(R.layout.fragment_about_us, frameLayout);
+        getLayoutInflater().inflate(R.layout.fragment_about_us, null);
+
+    }
+
+}
