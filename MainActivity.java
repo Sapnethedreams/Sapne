82caf5d9024964255d844c4d2054af04fc11fe66
Arjun Gupta <arjungupta3023@gmail.com>
2017-11-13 17:44:11 +0530

    Products

---
+++ MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.sapne;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+}
+}
