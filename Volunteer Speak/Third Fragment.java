edee886465991f05fe98533057326c7994bb1d5a
Arjun Gupta <arjungupta3023@gmail.com>
2017-11-21 19:31:22 +0530

    Create Third Fragment.java

---
+++ Volunteer Speak/Third Fragment.java
@@ -0,0 +1,34 @@
+package com.example.android.kaka;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ThirdFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.third_frag, container, false);
+
+        TextView tv = (TextView) v.findViewById(R.id.tvFragThird);
+        tv.setText(getArguments().getString("msg"));
+
+       //ImageView im = (ImageView) v.findViewById(R.id.imageView3);
+
+        return v;
+    }
+
+    public static ThirdFragment newInstance(String text) {
+
+        ThirdFragment f = new ThirdFragment();
+        Bundle b = new Bundle();
+        b.putString("msg", text);
+
+        f.setArguments(b);
+
+        return f;
+    }
+}
