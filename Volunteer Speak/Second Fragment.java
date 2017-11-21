d473bf75d4197c08e2a1ffde10cfac57223d0f1b
Arjun Gupta <arjungupta3023@gmail.com>
2017-11-21 19:29:10 +0530

    Create Second Fragment.java

---
+++ Volunteer Speak/Second Fragment.java
@@ -0,0 +1,34 @@
+package com.example.android.kaka;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class SecondFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.second_frag, container, false);
+
+        TextView tv = (TextView) v.findViewById(R.id.tvFragSecond);
+        tv.setText(getArguments().getString("msg"));
+
+        //ImageView im = (ImageView) v.findViewById(R.id.imageView2);
+
+        return v;
+    }
+
+    public static SecondFragment newInstance(String text) {
+
+        SecondFragment f = new SecondFragment();
+        Bundle b = new Bundle();
+        b.putString("msg", text);
+
+        f.setArguments(b);
+
+        return f;
+    }
+}
